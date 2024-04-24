6066da4092fb685ac121ccca0459f463068bf8c4
이지영 <abcd23589534@gmail.com>
2024-04-24 15:13:29 +0900

    [Fix] @Build 삭제 및 생성자 생성

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/domain/Letters.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/domain/Letters.java
@@ -2,14 +2,15 @@ package com.ssafy.bridgetalkback.letters.domain;
 
 import com.ssafy.bridgetalkback.global.BaseEntity;
 import com.ssafy.bridgetalkback.parent.domain.Parents;
+import com.ssafy.bridgetalkback.parent.domain.Role;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.UUID;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 @Table(name="letters")
 public class Letters extends BaseEntity {
 
@@ -29,6 +30,17 @@ public class Letters extends BaseEntity {
 
     @Column(columnDefinition = "integer default 0")
     private int isChecked;
+
+    public Letters(Parents parents, String lettersOriginalContent, String lettersTranslationContent, int isChecked) {
+        this.parents = parents;
+        this.lettersOriginalContent = lettersOriginalContent;
+        this.lettersTranslationContent = lettersTranslationContent;
+        this.isChecked = 0;
+    }
+
+    public static Letters createLetters(Parents parents, String lettersOriginalContent, String lettersTranslationContent, int isChecked) {
+        return new Letters(parents, lettersOriginalContent, lettersTranslationContent, isChecked);
+    }
 
 
 }
