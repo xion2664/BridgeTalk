cae7b369d543d66f5fee10113f19394cde3c0553
이지영 <abcd23589534@gmail.com>
2024-04-25 17:10:01 +0900

    [Feat] S3 config, service, test code 추가

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/controller/LettersController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/controller/LettersController.java
@@ -1,8 +1,10 @@
 package com.ssafy.bridgetalkback.letters.controller;
 
-//import com.ssafy.bridgetalkback.letters.service.STTService;
 import com.ssafy.bridgetalkback.letters.dto.request.LettersRequestDTO;
+import com.ssafy.bridgetalkback.letters.dto.response.LettersResponseDTO;
+import com.ssafy.bridgetalkback.letters.service.LettersService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value="/api/letters")
 @RequiredArgsConstructor
-
+@Slf4j
 public class LettersController {
+
+    private final LettersService lettersService;
 
     @GetMapping("/{lettersId}")
     public ResponseEntity<?> findLettersVoice(@PathVariable Long lettersId){
@@ -19,6 +23,14 @@ public class LettersController {
     }
     @PostMapping("/upload")
     public ResponseEntity<?> uploadLettersVoice(@ModelAttribute LettersRequestDTO lettersRequestDTO){
+        log.info("{ LettersController } : 부모 음성 편지 업로드 컨트롤러");
+        log.info("LetterRequestDTO : {}", lettersRequestDTO);
+
+        // 음성편지 s3 업로드
+        String voiceUrl = lettersService.saveVoiceFile(lettersRequestDTO);
+        // stt 실행
+        LettersResponseDTO responseDTO = lettersService.createText(voiceUrl);
+
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/dto/request/LettersRequestDTO.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/dto/request/LettersRequestDTO.java
@@ -1,7 +1,7 @@
 package com.ssafy.bridgetalkback.letters.dto.request;
 
 import com.ssafy.bridgetalkback.letters.domain.Letters;
-import com.ssafy.bridgetalkback.parent.domain.Parents;
+import com.ssafy.bridgetalkback.parents.domain.Parents;
 import com.ssafy.bridgetalkback.reports.domain.Reports;
 import org.springframework.web.multipart.MultipartFile;
 
