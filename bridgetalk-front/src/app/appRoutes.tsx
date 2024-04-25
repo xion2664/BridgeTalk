ea2e77a3d35f8f84e96ae7612ba9b344aa52f496
조한빈 <whgksqls9999@naver.com>
2024-04-25 10:49:54 +0900

    [feat] 부모 페이지 생성

--- bridgetalk-front/src/app/appRoutes.tsx
+++ bridgetalk-front/src/app/appRoutes.tsx
@@ -1,17 +1,44 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { TeestZustand, Test, TestCamera, TestDraw, TestPuzzle, TestVoice, TestWordcloud, ErrorPage } from '@/pages';
+import { Routes, Route } from 'react-router-dom';
+import {
+    Parent,
+    ParentInformation,
+    ParentInformationMain,
+    ParentInformationNews,
+    ParentInformationWord,
+    ParentMain,
+    ParentReportDetail,
+    ParentReportList,
+    TeestZustand,
+    Test,
+    TestCamera,
+    TestDraw,
+    TestPuzzle,
+    TestVoice,
+    TestWordcloud,
+    ErrorPage,
+} from '@/pages';
 
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Test />}>
-                <Route path="/" element={<Navigate to={'/camera'} />} />
+                <Route path="/" element={<TestCamera />} />
                 <Route path="camera" element={<TestCamera />} />
                 <Route path="draw" element={<TestDraw />} />
                 <Route path="puzzle" element={<TestPuzzle />} />
                 <Route path="voice" element={<TestVoice />} />
                 <Route path="wordcloud" element={<TestWordcloud />} />
                 <Route path="zustand" element={<TeestZustand />} />
+            </Route>
+            <Route path="/parent" element={<Parent />}>
+                <Route path="main" element={<ParentMain />}></Route>
+                <Route path="reportlist" element={<ParentReportList />}></Route>
+                <Route path="report/:report_id" element={<ParentReportDetail />}></Route>
+                <Route path="information" element={<ParentInformation />}>
+                    <Route path="main" element={<ParentInformationMain />}></Route>
+                    <Route path="news" element={<ParentInformationNews />}></Route>
+                    <Route path="word" element={<ParentInformationWord />}></Route>
+                </Route>
             </Route>
             <Route path="*" element={<ErrorPage />}></Route>
         </Routes>
