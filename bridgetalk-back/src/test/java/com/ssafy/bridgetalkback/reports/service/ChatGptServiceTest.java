365ccf9578a1c5ab720b9d84af5b7f60c9daf12e
hyun0-25 <lisa1072@skku.edu>
2024-04-26 10:07:35 +0900

    [Test] ChatGPT 번역 관련 테스트코드 작성

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/service/ChatGptServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/reports/service/ChatGptServiceTest.java
@@ -37,15 +37,33 @@ public class ChatGptServiceTest extends ServiceTest {
 
     @Test
     @DisplayName("ChatGPT로 원본텍스트의 요약본을 반환한다.")
-    void test() {
+    void summaryTest() {
+        //given
         String originText = "디노야, 오늘은 정말 놀이동산에 가고 싶어! 아침부터 엄마에게 계속해서 말했는데, 엄마는 항상 바쁘다며 내 얘기를 잘 못 알아들어줘. 나는 놀이동산에서 내가 좋아하는 모든 놀이기구를 타고 싶고, 큰 풍선도 사고 싶고, 푸른 하늘을 날고 싶어! 미끄럼틀에서 빠르게 내려가면서 내 친구들과 함께 즐거운 시간을 보내고 싶어! 그리고 맛있는 아이스크림도 먹고 싶어! 엄마야, 내가 놀이동산에 가고 싶은 이유를 알아줘! 나는 너무나도 설레고 기대돼. 그냥 빨리 놀이동산에 가고 싶어서 말이야!";
         reports = reportsRepository.save(Reports.createReports(kids, originText));
 
         //when
         String summary = chatGptService.createPrompt(reports.getReportsOriginContent());
+
         //then
         String result = "나는 놀이동산에 가고 싶어서 엄마에게 말했지만 항상 바쁜 엄마는 내 얘기를 잘 못 알아들어줘. 나는 놀이동산에서 모든 놀이기구를 타고 싶고, 친구들과 함께 즐거운 시간을 보내고 싶어! 아이스크림도 먹고 싶어!";
         assertThat(result).isEqualTo(summary);
+    }
+
+    @Test
+    @DisplayName("ChatGPT로 요약본을 번역한 텍스트를 반환한다.")
+    void translateTest() {
+        //given
+        String originText = "디노야, 오늘은 정말 놀이동산에 가고 싶어! 아침부터 엄마에게 계속해서 말했는데, 엄마는 항상 바쁘다며 내 얘기를 잘 못 알아들어줘. 나는 놀이동산에서 내가 좋아하는 모든 놀이기구를 타고 싶고, 큰 풍선도 사고 싶고, 푸른 하늘을 날고 싶어! 미끄럼틀에서 빠르게 내려가면서 내 친구들과 함께 즐거운 시간을 보내고 싶어! 그리고 맛있는 아이스크림도 먹고 싶어! 엄마야, 내가 놀이동산에 가고 싶은 이유를 알아줘! 나는 너무나도 설레고 기대돼. 그냥 빨리 놀이동산에 가고 싶어서 말이야!";
+        reports = reportsRepository.save(Reports.createReports(kids, originText));
+        String summary = chatGptService.createPrompt(reports.getReportsOriginContent());
+
+        //when
+        String translate = chatGptService.translatePrompt(summary);
+        String result = "Tôi muốn đi đến công viên giải trí và tôi đã nói với mẹ nhưng mẹ luôn bận rộn và không hiểu ý tôi. Tôi muốn cưỡi tất cả các trò chơi, mua bóng bay và có thời gian vui vẻ cùng bạn bè ở công viên giải trí!";
+
+        //then
+        assertThat(result).isEqualTo(translate);
     }
 
 
