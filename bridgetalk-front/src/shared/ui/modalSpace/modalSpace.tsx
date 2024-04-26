cda869981c599a44b12185766f907e9eea679658
조한빈 <whgksqls9999@naver.com>
2024-04-26 14:30:18 +0900

    [Feat] 녹음 완료 후 다음 페이지 이동

--- bridgetalk-front/src/shared/ui/modalSpace/modalSpace.tsx
+++ bridgetalk-front/src/shared/ui/modalSpace/modalSpace.tsx
@@ -1,36 +1,32 @@
 import { useVoiceStore } from '@/pages';
 import * as S from '@/styles/shared/modalSpace.style';
+import { useEffect, useState } from 'react';
 
 export function ModalSpace() {
-    return (
-        <>
-            <ParentVoiceRecordModalArea />
-        </>
-    );
+    const isRecordFinished = useVoiceStore((state) => state.isRecordFinished);
+
+    return <>{isRecordFinished && <ParentVoiceRecordModalArea />}</>;
 }
 
 function ParentVoiceRecordModalArea() {
     const setIsRecordFinished = useVoiceStore((state) => state.setIsRecordFinished);
-    const isRecordFinished = useVoiceStore((state) => state.isRecordFinished);
     const audioURL = useVoiceStore((state) => state.audioURL);
 
-    if (isRecordFinished) {
-        return (
-            <S.Container>
-                <div>
-                    <audio src={audioURL} controls></audio>
-                    <div style={{ display: 'flex', gap: '2svw' }}>
-                        <button
-                            onClick={() => {
-                                setIsRecordFinished(false);
-                            }}
-                        >
-                            Bỏ
-                        </button>
-                        <button>Gửi</button>
-                    </div>
+    return (
+        <S.Container>
+            <div>
+                <audio src={audioURL} preload={'auto'} controls></audio>
+                <div style={{ display: 'flex', gap: '2svw' }}>
+                    <button
+                        onClick={() => {
+                            setIsRecordFinished(false);
+                        }}
+                    >
+                        Bỏ
+                    </button>
+                    <button>Gửi</button>
                 </div>
-            </S.Container>
-        );
-    }
+            </div>
+        </S.Container>
+    );
 }
