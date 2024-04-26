e1ec962fdc5e697b776626c0556c4cd2bad37008
방소영 <bjo8244@naver.com>
2024-04-26 14:19:14 +0900

    [Feat] 편지 음성 조회 Controller 구현

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/controller/LettersController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/letters/controller/LettersController.java
@@ -1,22 +1,29 @@
 package com.ssafy.bridgetalkback.letters.controller;
 
+import com.ssafy.bridgetalkback.letters.service.LettersService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping(value="/api/letters")
+@RequestMapping(value = "/api/letters")
 @RequiredArgsConstructor
-
 public class LettersController {
 
+    private final LettersService lettersService;
+
     @GetMapping("/{lettersId}")
-    public ResponseEntity<?> findLettersVoice(@PathVariable Long lettersId){
-        return ResponseEntity.status(HttpStatus.OK).build();
+    public ResponseEntity<Resource> findLettersVoice(@PathVariable Long lettersId) {
+        log.info("{LetterController} : 편지 TTS 진입");
+        Resource mp3Resource = lettersService.findLettersVoice(lettersId);
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.parseMediaType("audio/mpeg")).body(mp3Resource);
     }
 }
