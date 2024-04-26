65e6ef0940daf6fdbf6f1770d49d9c02763ebcf5
sunkyoung <yskkjdh@gmail.com>
2024-04-26 11:21:04 +0900

    [Fix] test 시 서버 DB가 돌아가는 이슈 및 EmbeddedRedisConfig 인식 못하는 이슈 해결

--- bridgetalk-back/src/test/java/global/config/EmbeddedRedisConfig.java
+++ bridgetalk-back/src/test/java/global/config/EmbeddedRedisConfig.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,6 +12,7 @@ import redis.embedded.RedisServer;
 
 import java.io.IOException;
 
+@Profile("test")
 @TestConfiguration
 public class EmbeddedRedisConfig {
     private static final int port = 16379;
