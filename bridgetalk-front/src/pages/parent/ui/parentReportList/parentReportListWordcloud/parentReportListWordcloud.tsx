13a7387b532880f0a2a44d38379f9df99818991c
조한빈 <whgksqls9999@naver.com>
2024-04-29 15:28:32 +0900

    [Design] 워드클라우드 구현

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListWordcloud/parentReportListWordcloud.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListWordcloud/parentReportListWordcloud.tsx
@@ -1,10 +1,20 @@
 import * as S from '@/styles/parent/parentReportListWordcloud.style';
+import ReactWordcloud from 'react-wordcloud';
 
 export function ParentReportListWordcloud() {
     return (
         <S.Wrapper>
             <div className="title">키워드 분석</div>
-            <div className="cloud"></div>
+            <div className="cloud">
+                <ReactWordcloud
+                    words={[
+                        { text: '학교', value: 3 },
+                        { text: '친구', value: 5 },
+                        { text: '엄마', value: 2 },
+                    ]}
+                    options={{ fontFamily: 'DNF' }}
+                />
+            </div>
         </S.Wrapper>
     );
 }
