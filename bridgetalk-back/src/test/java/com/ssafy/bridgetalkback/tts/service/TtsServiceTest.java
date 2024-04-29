47a5f4a4f48f297d7013db379f80dad95033a304
방소영 <bjo8244@naver.com>
2024-04-29 16:40:23 +0900

    [Refactor] tts 실시간 스트리밍 반환으로 변경

--- bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/tts/service/TtsServiceTest.java
+++ bridgetalk-back/src/test/java/com/ssafy/bridgetalkback/tts/service/TtsServiceTest.java
@@ -1,16 +1,15 @@
 package com.ssafy.bridgetalkback.tts.service;
 
 import com.ssafy.bridgetalkback.common.ServiceTest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -26,44 +25,23 @@ public class TtsServiceTest extends ServiceTest {
 
     private HttpURLConnection mockConnection;
     private URL mockUrl;
-    private InputStream mockInputStream;
 
     @BeforeEach
     void setup() throws Exception {
         mockUrl = mock(URL.class);
         mockConnection = mock(HttpURLConnection.class);
-        mockInputStream = new ByteArrayInputStream("테스트 오디오 파일".getBytes());
-
         when(mockUrl.openConnection()).thenReturn(mockConnection);
-        when(mockConnection.getInputStream()).thenReturn(mockInputStream);
-
-        ReflectionTestUtils.setField(ttsService, "url", mockUrl);
+        when(mockConnection.getInputStream()).thenReturn(new ByteArrayInputStream("임시 오디오 데이터".getBytes()));
+        when(mockConnection.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
     }
 
     @Test
-    @DisplayName("Text-to-Speech 변환 성공")
-    void testTextToSpeechSuccess() throws Exception {
-        // Given
-        when(mockConnection.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
+    @DisplayName("Text-to-Speech 스트리밍 성공 검증")
+    void testTextToSpeechStreamingSuccess() throws Exception {
+        StreamingResponseBody responseBody = ttsService.textToSpeech("테스트할 임시 텍스트 데이터입니다.");
+        MockHttpServletResponse mockResponse = new MockHttpServletResponse();
+        responseBody.writeTo(mockResponse.getOutputStream());
 
-        // When
-        Resource response = ttsService.textToSpeech("테스트할 임시 텍스트 데이터입니다.");
-
-        // Then
-        assertThat(response).isInstanceOf(FileSystemResource.class);
-    }
-
-    @Test
-    @DisplayName("변환된 파일이 mp3 형식인지 확인")
-    void testFileIsMp3Format() throws Exception {
-        // Given
-        when(mockConnection.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
-
-        // When
-        Resource response = ttsService.textToSpeech("테스트할 임시 텍스트 데이터입니다.");
-
-        // Then
-        FileSystemResource fsr = (FileSystemResource) response;
-        assertThat(fsr.getFilename()).endsWith(".mp3");
+        assertThat(mockResponse.getStatus()).isEqualTo(HttpServletResponse.SC_OK);
     }
 }
