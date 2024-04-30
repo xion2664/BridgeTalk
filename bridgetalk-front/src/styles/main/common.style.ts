d1a9e59df3cd792cf6257415d3b795e3001be368
조한빈 <whgksqls9999@naver.com>
2024-04-30 10:50:55 +0900

    [Design] 메인 페이지 공통 스타일 설정

---
+++ bridgetalk-front/src/styles/main/common.style.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+/**
+ * 메인 페이지 색상 팔레트
+ * 사용예: ${color(0.5).main}
+ * @param alpha
+ * @returns
+ */
+export function color(alpha: number) {
+  return {
+    main: `rgba(255, 97, 97, ${alpha})`,
+    sub: `rgba(255, 159, 153, ${alpha})`,
+    line: `rgba(255, 232, 232, ${alpha})`,
+    light: `rgba(255, 232, 232, ${alpha})`,
+    gray: `rgba(170, 170, 170, ${alpha})`,
+    white: `rgba(255, 255, 255, ${alpha})`,
+    dark: `rgba(64, 64, 64, ${alpha})`,
+    black: `rgba(17, 17, 17, ${alpha})`,
+  };
+}
+
+export const bg = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100svw;
+  height: 100svh;
+`;
