da9f7a8b84079968765dca4650971747485bf201
조한빈 <whgksqls9999@naver.com>
2024-05-01 09:21:39 +0900

    [Style] TabWidth 조정

--- bridgetalk-front/src/pages/parent/query/postVoiceBlob/postVoiceBlob.tsx
+++ bridgetalk-front/src/pages/parent/query/postVoiceBlob/postVoiceBlob.tsx
@@ -1,5 +1,10 @@
 import { customAxios } from '@/shared';
 
+/**
+ * postVoiceBlob() : 녹음 파일 전송
+ * @param reportId : 답장할 리포트 번호
+ * @param voice : 음성 녹음 파일
+ */
 export function postVoiceBlob(reportId: number, voice: Blob) {
   const formData = new FormData();
 
