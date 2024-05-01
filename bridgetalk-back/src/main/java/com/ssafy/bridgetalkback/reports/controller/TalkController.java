d91afea650e34e8b4dfa7d186ac2654d7d1d5c9c
방소영 <bjo8244@naver.com>
2024-05-01 23:31:21 +0900

    [Feat] 대화 시작 멘트 TTS 전송 API 기능 구현

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
@@ -1,6 +1,7 @@
 package com.ssafy.bridgetalkback.reports.controller;
 
 import com.ssafy.bridgetalkback.global.annotation.ExtractPayload;
+import com.ssafy.bridgetalkback.reports.service.ReportsService;
 import com.ssafy.bridgetalkback.reports.service.TalkService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @Slf4j
-@Tag(name = "talk", description = "TalkController")@RestController
+@Tag(name = "talk", description = "TalkController")
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/reports")
 public class TalkController {
     private final TalkService talkService;
+    private final ReportsService reportsService;
 
     @GetMapping("/talk-stop")
     public ResponseEntity<Resource> stopTalk(@ExtractPayload String userId) {
@@ -28,4 +31,13 @@ public class TalkController {
                 .contentType(MediaType.parseMediaType("audio/mpeg"))
                 .body(talkService.stopTalk(UUID.fromString(userId)));
     }
+
+    @GetMapping("/talk-start")
+    public ResponseEntity<Resource> startTalk(@ExtractPayload String userId) {
+        log.info("{ TalkController } : 대화 시작 진입");
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("audio/mpeg"))
+                .body(talkService.startTalk(UUID.fromString(userId)));
+    }
+
 }
