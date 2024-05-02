773fdb947269e7211540470ca7ecbddbbffbd149
sunkyoung <yskkjdh@gmail.com>
2024-05-02 12:06:45 +0900

    [Feat] TalkController 대화하기 API 구현

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/reports/controller/TalkController.java
@@ -1,6 +1,7 @@
 package com.ssafy.bridgetalkback.reports.controller;
 
 import com.ssafy.bridgetalkback.global.annotation.ExtractPayload;
+import com.ssafy.bridgetalkback.reports.service.ReportsService;
 import com.ssafy.bridgetalkback.reports.service.TalkService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -8,9 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
@@ -20,6 +20,7 @@ import java.util.UUID;
 @RequestMapping("/api/reports")
 public class TalkController {
     private final TalkService talkService;
+    private final ReportsService reportsService;
 
     @GetMapping("/talk-stop")
     public ResponseEntity<Resource> stopTalk(@ExtractPayload String userId) {
@@ -27,5 +28,22 @@ public class TalkController {
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType("audio/mpeg"))
                 .body(talkService.stopTalk(UUID.fromString(userId)));
+    }
+
+    @PatchMapping("/talk-send/{reportsId}")
+    public ResponseEntity<Resource> sendTalk(@ExtractPayload String userId, @PathVariable Long reportsId,
+                                             @RequestPart(value = "reportsFile") MultipartFile multipartFile) {
+        log.info("{ TalkController } : 대화 하기 진입");
+
+        // 아이 음성 파일 업로드 및 stt
+        String talkText = reportsService.createText(multipartFile);
+
+        // 변환 텍스트 포함 하도록 DB 원본 레포트 업데이트
+        String reportsText = reportsService.updateOriginContent(UUID.fromString(userId), reportsId, talkText);
+
+        // 변환 텍스트에 대한 답장 생성 및 tts로 변환
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("audio/mpeg"))
+                .body(talkService.sendTalk(UUID.fromString(userId), reportsText));
     }
 }
