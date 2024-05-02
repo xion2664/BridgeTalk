2bf78dcfcabfa86d4d2b37d0cde3873eeceab1cb
조한빈 <whgksqls9999@naver.com>
2024-05-02 11:23:43 +0900

    [Feat] 부모 메인 페이지에서 자녀 정보 파싱하기

--- bridgetalk-front/src/pages/parent/ui/parentMain/parentMain.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentMain/parentMain.tsx
@@ -2,15 +2,27 @@ import { customAxios } from '@/shared';
 import * as S from '@/styles/parent/parentMain.style';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useReportStore } from '../../store';
 
 export function ParentMain() {
   const navigate = useNavigate();
+
+  const setChildrenList = useReportStore((state) => state.childrenList);
 
   useEffect(() => {
     customAxios
       .get('/profile')
       .then((res) => {
-        console.log(res);
+        // 부모의 프로필에서 자녀들의 정보를 빼 childrneList에 저장
+        const childrenList: any = [];
+        const profileList = res.data.profileList;
+
+        for (let i = 1; i < profileList.length; i++) {
+          childrenList.push(profileList[i]);
+        }
+
+        setChildrenList(childrenList);
+        console.log(childrenList);
       })
       .catch((err) => console.log(err));
   }, []);
