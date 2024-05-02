07b27a3ed2b880c7dc4cd43a05d3bbee04f2d247
조한빈 <whgksqls9999@naver.com>
2024-05-03 01:07:00 +0900

    [Feat] 액세스, 리프레시 토큰 저장

--- bridgetalk-front/src/pages/main/model/handleSignin/handleSignin.ts
+++ bridgetalk-front/src/pages/main/model/handleSignin/handleSignin.ts
@@ -1,14 +1,21 @@
 import { postSignin } from '../../query';
+import { useUserStore } from '../../store';
 
 export function handleSignin(requestDto: any) {
   postSignin(requestDto)
     .then((res: any) => {
       if (res.status === 200) {
         console.log(res.data);
-        sessionStorage.setItem('token', res.data.accessToken);
-      } else {
-        alert('로그인 정보가 일치하지 않습니다.');
+        const data = res.data;
+        sessionStorage.setItem(
+          btoa('access' + process.env.REACT_APP_SECURE_CODE),
+          btoa(data.accessToken + process.env.REACT_APP_SECURE_CODE),
+        );
+        sessionStorage.setItem(
+          btoa('refresh' + process.env.REACT_APP_SECURE_CODE),
+          btoa(data.refreshToken + process.env.REACT_APP_SECURE_CODE),
+        );
       }
     })
-    .catch((err) => alert('로그인 정보가 일치하지 않습니다.'));
+    .catch((err) => alert('로그인 정보가 일치하지 않습니다.' + err));
 }
