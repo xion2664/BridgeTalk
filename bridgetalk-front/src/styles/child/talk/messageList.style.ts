6ac4abb1ff5054cdf6657ca5cc44022380877f97
박시연 <xion2664@gmail.com>
2024-05-03 14:50:36 +0900

    [Feat] message UI

--- bridgetalk-front/src/styles/child/talk/messageList.style.ts
+++ bridgetalk-front/src/styles/child/talk/messageList.style.ts
@@ -1,24 +1,67 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  .messageList {
-    &__header {
-      display: flex;
-      align-item: center;
+  width: 100svw;
 
-      img {
-        width: 8svw;
+  * {
+    box-sizing: border-box;
+
+    ::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  .messageList {
+    display: flex;
+    align-item: center;
+    justify-content: baseline;
+    gap: 10svh;
+
+    &__list {
+      width: 60svw;
+      height: 60svh;
+      padding: 3svh;
+
+      border-radius: 5svh;
+      background-color: #ff9f99;
+
+      &-header {
+        display: flex;
+        align-item: center;
+
+        img {
+          width: 8svw;
+        }
+      }
+
+      &-content {
+        display: flex;
+        flex-direction: column;
+        gap: 1svh;
+
+        height: 35svh;
+        overflow: scroll;
       }
     }
 
-    &__container {
-      &-list {
+    &__info {
+      display: flex;
+      flex-direction: column;
+      align-item: center;
+      justify-content: center;
+
+      span {
+        padding: 4svh;
+
+        font-size: 3svh;
+        font-family: 'DNF';
+
+        border-radius: 10svh;
+        background-color: white;
       }
 
-      &-info {
-        img {
-          height: 20svh;
-        }
+      img {
+        height: 20svh;
       }
     }
   }
