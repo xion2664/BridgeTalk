4a43fd8d3cfb8f889ae3d3182a82c6089a6101e4
조한빈 <whgksqls9999@naver.com>
2024-05-03 12:31:35 +0900

    [Feat] 토큰 디코딩

--- bridgetalk-front/src/shared/util/customAxios.ts
+++ bridgetalk-front/src/shared/util/customAxios.ts
@@ -1,15 +1,14 @@
 import axios, { AxiosInstance } from 'axios';
 import { getCookie } from './cookie';
 import Swal from 'sweetalert2';
+import { decodeToken } from '../model';
 
 export const customAxios: AxiosInstance = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}`,
   headers: {
-    Authorization:
-      'Bearer ' +
-      atob(sessionStorage.getItem(btoa('access' + process.env.REACT_APP_SECURE_CODE))!).split(
-        `${process.env.REACT_APP_SECURE_CODE}`,
-      )[0],
+    Authorization: sessionStorage.getItem(btoa('access' + process.env.REACT_APP_SECURE_CODE))
+      ? 'Bearer ' + decodeToken('access')
+      : '',
   },
 });
 
