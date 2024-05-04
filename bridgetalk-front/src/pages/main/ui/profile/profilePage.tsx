2f12b18bc56b3e100550412b21cd77a4dfaded83
조한빈 <whgksqls9999@naver.com>
2024-05-04 12:37:10 +0900

    [Feat] 프로필리스트 로딩 상태 관리

--- bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
+++ bridgetalk-front/src/pages/main/ui/profile/profilePage.tsx
@@ -17,6 +17,7 @@ export function ProfilePage() {
   const navigate = useNavigate();
 
   const [profileList, setProfileList] = useState<Profile[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const { refreshToken, accessToken } = useUserStore((state) => ({
     refreshToken: state.refreshToken,
@@ -39,6 +40,12 @@ export function ProfilePage() {
     }
   }, []);
 
+  useEffect(() => {
+    if (profileList.length > 0) {
+      setIsLoading(false);
+    }
+  }, [profileList]);
+
   return (
     <S.Container>
       <button
@@ -55,66 +62,70 @@ export function ProfilePage() {
       <button className="setting">
         <img src={'assets/img/main/setting.svg'} />
       </button>
-      <div className="main">
-        <div className="main__title">
-          <img src={'assets/img/main/profile.svg'} />
-        </div>
-        <div className="main__profilelist">
-          {profileList.length > 0 &&
-            profileList.map((it, idx) => (
-              <div className="main__profilelist-item" key={it.userId}>
+      {!isLoading && (
+        <>
+          <div className="main">
+            <div className="main__title">
+              <img src={'assets/img/main/profile.svg'} />
+            </div>
+            <div className="main__profilelist">
+              {profileList.length > 0 &&
+                profileList.map((it, idx) => (
+                  <div className="main__profilelist-item" key={it.userId}>
+                    <button
+                      className="main__profilelist-item-edit"
+                      onClick={() => {
+                        navigate('/editProfile');
+                      }}
+                    >
+                      <img src={'assets/img/main/editProfileIcon.svg'} />
+                    </button>
+                    <div className="main__profilelist-item-dino">
+                      <img
+                        src={`assets/img/${it.userDino}.svg`}
+                        alt="캐릭터"
+                        onClick={() => {
+                          postProfileLogin(it.userId).then((res) => {
+                            if (res && res.data) {
+                              setToken(res.data.accessToken, res.data.refreshToken);
+                            }
+                          });
+
+                          if (idx > 0) {
+                            navigate('/child');
+                          } else {
+                            navigate('/parent');
+                          }
+                        }}
+                      />
+                    </div>
+                    <div className="main__profilelist-item-title">{it.userName}</div>
+                  </div>
+                ))}
+              <div className="main__profilelist-empty">
                 <button
-                  className="main__profilelist-item-edit"
                   onClick={() => {
-                    navigate('/editProfile');
+                    navigate('/addProfile');
                   }}
                 >
-                  <img src={'assets/img/main/editProfileIcon.svg'} />
+                  <img src={'assets/img/main/addProfile.svg'} />
                 </button>
-                <div className="main__profilelist-item-dino">
-                  <img
-                    src={`assets/img/${it.userDino}.svg`}
-                    alt="캐릭터"
-                    onClick={() => {
-                      postProfileLogin(it.userId).then((res) => {
-                        if (res && res.data) {
-                          setToken(res.data.accessToken, res.data.refreshToken);
-                        }
-                      });
-
-                      if (idx > 0) {
-                        navigate('/child');
-                      } else {
-                        navigate('/parent');
-                      }
-                    }}
-                  />
-                </div>
-                <div className="main__profilelist-item-title">{it.userName}</div>
               </div>
-            ))}
-          <div className="main__profilelist-empty">
-            <button
-              onClick={() => {
-                navigate('/addProfile');
-              }}
-            >
-              <img src={'assets/img/main/addProfile.svg'} />
-            </button>
+            </div>
+            <div className="main__button">
+              <button
+                className="main__button-start"
+                onClick={() => {
+                  navigate('/parent');
+                  // 스타트 버튼 눌렀을 때 임시로 부모 페이지로 링크
+                }}
+              >
+                START!
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="main__button">
-          <button
-            className="main__button-start"
-            onClick={() => {
-              navigate('/parent');
-              // 스타트 버튼 눌렀을 때 임시로 부모 페이지로 링크
-            }}
-          >
-            START!
-          </button>
-        </div>
-      </div>
+        </>
+      )}
     </S.Container>
   );
 }
