1dff9938847c9b916d383e8ce378d4083b6eeebb
방소영 <bjo8244@naver.com>
2024-05-08 08:08:13 +0900

    [Refactor] Slang 생성자 및 NOTNULL 설정

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/slang/domain/Slang.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/slang/domain/Slang.java
@@ -1,10 +1,7 @@
 package com.ssafy.bridgetalkback.slang.domain;
 
 import com.ssafy.bridgetalkback.global.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,15 +16,30 @@ public class Slang extends BaseEntity {
     @GeneratedValue
     private Long slangId;
 
+    @Column(nullable = false)
     private String slangWord;
 
     private String originalWord;
 
+    @Column(nullable = false)
     private String meaning;
 
+    @Column(nullable = false)
     private String vietnamesePronunciation;
 
+    @Column(nullable = false)
     private String vietnameseTranslation;
 
-    // 생성자 만들 필요 없음
+    private Slang(String slangWord, String originalWord, String meaning, String vietnamesePronunciation, String vietnameseTranslation) {
+        this.slangWord = slangWord;
+        this.originalWord = originalWord;
+        this.meaning = meaning;
+        this.vietnamesePronunciation = vietnamesePronunciation;
+        this.vietnameseTranslation = vietnameseTranslation;
+    }
+
+    public static Slang createSlang(String slangWord, String originalWord, String meaning, String vietnamesePronunciation, String vietnameseTranslation) {
+        return new Slang(slangWord, originalWord, meaning, vietnamesePronunciation, vietnameseTranslation);
+    }
+
 }
