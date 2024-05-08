6cafec65e45367d460c13c3d4e934cc23a4f77cc
조한빈 <whgksqls9999@naver.com>
2024-05-08 15:46:59 +0900

    [Docs] public 폴더 내 FFmpeg load시 필요 파일 저장

--- bridgetalk-front/src/pages/child/query/postSendTalk/postSendTalk.ts
+++ bridgetalk-front/src/pages/child/query/postSendTalk/postSendTalk.ts
@@ -5,30 +5,26 @@ import { fetchFile, toBlobURL } from '@ffmpeg/util';
 export async function postSendTalk(reportsId: number, audio: Blob, setReply: any) {
   const formData = new FormData();
 
-  const file: any = new File([audio], 'input.webm', { type: 'audio/webm' });
+  // Blob => FileData
   const fileData = await audio.arrayBuffer();
   const newFileData = new Uint8Array(fileData);
 
   const ffmpeg = new FFmpeg();
 
-  const baseURL = '/@ffmpeg/core/dist/esm';
+  await ffmpeg.load();
 
-  await ffmpeg.load({
-    coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
-    wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-  });
-
+  // webm => mp3 변환
   await ffmpeg.writeFile('input.webm', newFileData);
-  console.log('{writeFile}');
+  console.log('{posetSendTalk: writeFile}');
   await ffmpeg.exec(['-i', 'input.webm', '-vn', '-ab', '192k', 'output.mp3']);
-  console.log('{exec}');
+  console.log('{posetSendTalk: exec}');
   const data: any = await ffmpeg.readFile('output.mp3');
-  console.log('{readFile}');
+  console.log('{posetSendTalk: readFile}');
   const newBlob = new Blob([data.buffer], { type: 'audio/mpeg' });
-  console.log('{create new Blob}');
+  console.log('{posetSendTalk: create new Blob}');
 
   formData.append('reportsFile', newBlob);
-  console.log(newBlob);
+
   return customAxios
     .patch(`/reports/talk-send/${reportsId}`, formData, {
       responseType: 'blob',
