d906441f662cdbf3f17eda45c99537a4545a8268
조한빈 <whgksqls9999@naver.com>
2024-05-08 11:03:37 +0900

    [Feat] 슬랭 조회 함수 작성

--- bridgetalk-front/src/pages/parent/ui/parent/parent.tsx
+++ bridgetalk-front/src/pages/parent/ui/parent/parent.tsx
@@ -3,12 +3,17 @@ import * as S from '@/styles/parent/parent.style';
 import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useReportStore } from '../../store';
+import { getSlang } from '../../query';
 
 export function Parent() {
   const navigate = useNavigate();
 
   useEffect(() => {
     navigate('main');
+
+    getSlang().then((res) => {
+      console.log(res);
+    });
   }, []);
 
   return (
