d08055c497987660944c9b3488efc0ae6d9ab1f2
sunkyoung <yskkjdh@gmail.com>
2024-05-10 15:13:11 +0900

    [Feat] 육아정보 크롤링 API 구현

---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/controller/ParentingInfoController.java
@@ -0,0 +1,32 @@
+package com.ssafy.bridgetalkback.parentingInfo.controller;
+
+import com.ssafy.bridgetalkback.global.annotation.ExtractPayload;
+import com.ssafy.bridgetalkback.parentingInfo.service.ParentingInfoCrawlingService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Tag(name = "ParentingInfo", description = "ParentingInfoController")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/parentingInfo")
+public class ParentingInfoController {
+    private final ParentingInfoCrawlingService crawlingService;
+
+    @GetMapping("/crawling")
+    public ResponseEntity<Void> crawling(@ExtractPayload String userId) throws Exception {
+        crawlingService.startParentingInfoCrawling();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> parentingInfoList(@ExtractPayload String userId) throws Exception {
+        crawlingService.startParentingInfoCrawling();
+        return ResponseEntity.ok().build();
+    }
+}
