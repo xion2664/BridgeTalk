55bf2147fa8d52873d82df5f4e28c662c9611cb6
조한빈 <whgksqls9999@naver.com>
2024-05-11 01:08:00 +0900

    [Feat] 토큰 저장 위치 변경

--- bridgetalk-front/src/pages/main/model/handleSignin/handleSignin.ts
+++ bridgetalk-front/src/pages/main/model/handleSignin/handleSignin.ts
@@ -7,11 +7,11 @@ export function handleSignin(requestDto: any, userStore: any, navigate: Navigate
       if (res.status === 200) {
         const data = res.data;
         // base64 인코딩
-        sessionStorage.setItem(
+        localStorage.setItem(
           btoa('access' + process.env.REACT_APP_SECURE_CODE),
           btoa(data.accessToken + process.env.REACT_APP_SECURE_CODE),
         );
-        sessionStorage.setItem(
+        localStorage.setItem(
           btoa('refresh' + process.env.REACT_APP_SECURE_CODE),
           btoa(data.refreshToken + process.env.REACT_APP_SECURE_CODE),
         );
