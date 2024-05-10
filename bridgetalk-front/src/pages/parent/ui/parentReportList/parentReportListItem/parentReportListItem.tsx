9e4229416bb23e2a0fca9fb900a887d0c0b14f9f
조한빈 <whgksqls9999@naver.com>
2024-05-10 11:12:39 +0900

    [Feat] 리포트 리스트 언어별 조건부 렌더링

--- bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListItem/parentReportListItem.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentReportList/parentReportListItem/parentReportListItem.tsx
@@ -1,5 +1,6 @@
+import { useReportStore } from '@/pages/parent/store';
 import * as S from '@/styles/parent/parentReportListItem.style';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 interface Props {
@@ -23,6 +24,15 @@ export function ParentReportListItem({
 }: Props) {
   const navigate = useNavigate();
   const [date, setDate] = useState<string[]>([]);
+  const langauge = useReportStore((state) => state.language);
+
+  const dateWord = useMemo(
+    () => ({
+      kor: ['년', '월', '일'],
+      viet: ['Năm', 'tháng', 'ngày'],
+    }),
+    [],
+  );
 
   useEffect(() => {
     setDate(createdAt.split('T')[0].split('-'));
@@ -32,16 +42,21 @@ export function ParentReportListItem({
     <S.Container onClick={() => navigate(`${reportsId}`)}>
       <S.Content>
         <S.ContentHeader>
-          <div>{date && `${date[0]}년 ${date[1]}월 ${date[2]}일`}</div>
+          <div style={{ fontFamily: langauge === 'kor' ? 'DNF' : 'Pretendard' }}>
+            {date &&
+              `${date[0]}${dateWord[langauge][0]} ${date[1]}${dateWord[langauge][1]} ${date[2]}${dateWord[langauge][2]}`}
+          </div>
           <div className="tags">
             {reportsKeywords.map((keyword, idx) => (
-              <div className="tag" key={idx}>
+              <div className="tag" key={idx} style={{ fontFamily: langauge === 'kor' ? 'DNF' : 'Pretendard' }}>
                 #{keyword}
               </div>
             ))}
           </div>
         </S.ContentHeader>
-        <S.ContentBody>{reportsSummary}</S.ContentBody>
+        <S.ContentBody style={{ fontFamily: langauge === 'kor' ? 'DNF' : 'Pretendard' }}>
+          {reportsSummary}
+        </S.ContentBody>
       </S.Content>
     </S.Container>
   );
