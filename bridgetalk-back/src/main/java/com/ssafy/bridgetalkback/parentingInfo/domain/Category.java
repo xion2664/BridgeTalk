6d860d6252bf55b8b941b7788a5a9720029f3fb6
sunkyoung <yskkjdh@gmail.com>
2024-05-11 23:32:33 +0900

    [Feat] 없는 카테고리로 조회 요청 시 예외 처리 작성

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/domain/Category.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/parentingInfo/domain/Category.java
@@ -1,9 +1,13 @@
 package com.ssafy.bridgetalkback.parentingInfo.domain;
 
+import com.ssafy.bridgetalkback.global.exception.BaseException;
 import com.ssafy.bridgetalkback.global.utils.EnumConverter;
 import com.ssafy.bridgetalkback.global.utils.EnumStandard;
+import com.ssafy.bridgetalkback.parentingInfo.exception.ParentingInfoErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
@@ -14,17 +18,24 @@ public enum Category implements EnumStandard {
     PUBERTY("puberty")
     ;
 
-    private final String age;
+    private final String category;
 
     @Override
     public String getValue() {
-        return age;
+        return category;
     }
 
     @jakarta.persistence.Converter
-    public static class AgeConverter extends EnumConverter<Category> {
-        public AgeConverter() {
+    public static class CategoryConverter extends EnumConverter<Category> {
+        public CategoryConverter() {
             super(Category.class);
         }
+    }
+
+    public static Category from(String value) {
+        return Arrays.stream(values())
+                .filter(category -> category.getValue().equals(value))
+                .findFirst()
+                .orElseThrow(() -> BaseException.type(ParentingInfoErrorCode.CATEGORY_NOT_FOUND));
     }
 }
