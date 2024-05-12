34243442bdc3a09e3b747c16bc8b45d47e61d12e
xion <xion2664@gmail.com>
2024-05-13 08:39:25 +0900

    [Feat] loading pages but wrong branch

--- bridgetalk-front/src/pages/child/ui/game/puzzle/puzzlePage.tsx
+++ bridgetalk-front/src/pages/child/ui/game/puzzle/puzzlePage.tsx
@@ -1,8 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { customAxios } from '@/shared';
 import { JigsawPuzzle } from 'react-jigsaw-puzzle/lib';
 import 'react-jigsaw-puzzle/lib/jigsaw-puzzle.css';
+import * as S from '@/styles/child/game/puzzle.style';
 
 export function PuzzlePage() {
+  const { id } = useParams();
+  const [puzzle, setPuzzle] = useState('');
+
+  useEffect(() => {
+    const fetchPuzzle = async () => {
+      try {
+        const response = await customAxios.get(`/puzzle/${id}`, {});
+        setPuzzle(response.data.puzzleImageUrl);
+        console.log('API Response Status Code:', response.status); // 콘솔에 상태 코드 출력
+      } catch (error) {
+        console.error('Failed to fetch puzzle data:', error);
+      }
+    };
+
+    if (id) {
+      fetchPuzzle();
+    }
+  }, [id]);
+
   return (
-    <JigsawPuzzle imageSrc="/assets/img/pic/halongbay.jpg" rows={3} columns={4} onSolved={() => alert('Solved!')} />
+    <S.Container>
+      <div className="puzzlePage">
+        <div className="puzzlePage__side">
+          <img src={puzzle} alt="" />
+        </div>
+        <div className="puzzlePage__puzzle">
+          <JigsawPuzzle imageSrc={puzzle} rows={3} columns={4} onSolved={() => alert('Solved!')} />
+        </div>
+      </div>
+    </S.Container>
   );
 }
