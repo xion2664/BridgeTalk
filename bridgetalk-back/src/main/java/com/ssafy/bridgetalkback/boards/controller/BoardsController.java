571e89b01d72b13e6293150de9f7bb46945e542b
sunkyoung <yskkjdh@gmail.com>
2024-05-14 01:38:29 +0900

    [Feat] (커뮤니티) 게시글 리스트 조회 API 구현

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/controller/BoardsController.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/controller/BoardsController.java
@@ -1,6 +1,9 @@
 package com.ssafy.bridgetalkback.boards.controller;
 
 import com.ssafy.bridgetalkback.boards.dto.response.BoardsResponseDto;
+import com.ssafy.bridgetalkback.boards.dto.response.CustomBoardsListResponseDto;
+import com.ssafy.bridgetalkback.boards.query.dto.BoardsListDto;
+import com.ssafy.bridgetalkback.boards.service.BoardsListService;
 import com.ssafy.bridgetalkback.boards.service.BoardsService;
 import com.ssafy.bridgetalkback.global.Language;
 import com.ssafy.bridgetalkback.global.annotation.ExtractPayload;
@@ -19,10 +22,21 @@ import java.util.UUID;
 @RequestMapping("/api/boards")
 public class BoardsController {
     private final BoardsService boardsService;
+    private final BoardsListService boardsListService;
 
     @GetMapping("/{boardId}/{language}")
     public ResponseEntity<BoardsResponseDto> getParentingInfoDetail(@ExtractPayload String userId, @PathVariable Long boardId, @PathVariable Language language) {
         log.info("{ BoardsController } : 게시글 상세조회 진입");
         return ResponseEntity.ok(boardsService.getBoardsDetail(UUID.fromString(userId), boardId, language));
+    }
+
+    @GetMapping("/{language}")
+    public ResponseEntity<CustomBoardsListResponseDto<BoardsListDto>> getCustomBoardsList(@ExtractPayload String userId, @PathVariable Language language,
+                                                                                          @RequestParam(value = "page", defaultValue = "0", required = false) int page,
+                                                                                          @RequestParam(value = "searchType", required = false, defaultValue = "title_and_content_and_reports") String searchType,
+                                                                                          @RequestParam(value = "searchWord", required = false, defaultValue = "") String searchWord,
+                                                                                          @RequestParam(value = "sort", required = false, defaultValue = "최신순") String sort) {
+        log.info("{ BoardsController } : 게시글 리스트조회 진입 (검색조건, 검색키워드, 정렬 유형) - " + searchType + "," + searchWord + "," + sort);
+        return ResponseEntity.ok(boardsListService.getCustomBoardsList(UUID.fromString(userId), page, searchType, searchWord, sort, language));
     }
 }
---
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/boards/query/BoardsListQueryRepositoryImpl.java
@@ -0,0 +1,142 @@
+package com.ssafy.bridgetalkback.boards.query;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.bridgetalkback.boards.domain.BoardsSearchType;
+import com.ssafy.bridgetalkback.boards.dto.response.CustomBoardsListResponseDto;
+import com.ssafy.bridgetalkback.boards.query.dto.BoardsListDto;
+import com.ssafy.bridgetalkback.boards.query.dto.QBoardsListDto;
+import com.ssafy.bridgetalkback.global.Language;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.querydsl.core.types.ExpressionUtils.count;
+import static com.ssafy.bridgetalkback.boards.domain.QBoards.boards;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class BoardsListQueryRepositoryImpl implements BoardsListQueryRepository {
+    private final JPAQueryFactory query;
+
+    @Override
+    public CustomBoardsListResponseDto<BoardsListDto> getBoardsListOrderByTime(int page, BoardsSearchType boardSearchType,
+                                                                               String searchWord, Language language) {
+        Pageable pageable = PageRequest.of(page, 10);
+        List<BoardsListDto> boardLists = query
+                .selectDistinct(createQBoardsListDto(language))
+                .from(boards)
+                .where(language.equals(Language.kor) ? searchKorea(boardSearchType, searchWord) : searchVietnam(boardSearchType, searchWord))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(boards.createdAt.desc())
+                .fetch();
+
+        JPAQuery<Long> countQuery = query
+                .select(count(boards.boardsId))
+                .from(boards)
+                .where(language.equals(Language.kor) ? searchKorea(boardSearchType, searchWord) : searchVietnam(boardSearchType, searchWord));
+
+        return new CustomBoardsListResponseDto<>(PageableExecutionUtils.getPage(boardLists, pageable, countQuery::fetchOne));
+    }
+
+    @Override
+    public CustomBoardsListResponseDto<BoardsListDto> getBoardsListOrderByLikes(int page, BoardsSearchType boardSearchType,
+                                                                                String searchWord, Language language) {
+        Pageable pageable = PageRequest.of(page, 10);
+        List<BoardsListDto> boardLists = query
+                .selectDistinct(createQBoardsListDto(language))
+                .from(boards)
+                .where(language.equals(Language.kor) ? searchKorea(boardSearchType, searchWord) : searchVietnam(boardSearchType, searchWord))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(boards.likes.desc())
+                .fetch();
+
+        JPAQuery<Long> countQuery = query
+                .select(count(boards.boardsId))
+                .from(boards)
+                .where(language.equals(Language.kor) ? searchKorea(boardSearchType, searchWord) : searchVietnam(boardSearchType, searchWord));
+
+        return new CustomBoardsListResponseDto<>(PageableExecutionUtils.getPage(boardLists, pageable, countQuery::fetchOne));
+    }
+
+    private BooleanExpression searchKorea(BoardsSearchType boardSearchType, String searchWord) {
+        log.info(" { BoardsListQueryRepositoryImpl } : searchKorea"+boardSearchType);
+        if (searchWord == null || searchWord.isEmpty()) {
+            return null;
+        } else {
+            switch (boardSearchType) {
+                case TITLE -> {
+                    log.info(" { BoardsListQueryRepositoryImpl } : "+searchWord);
+                    return boards.boardsTitle_kor.contains(searchWord);
+                }
+                case CONTENT_AND_REPORTS_SUMMARY -> {
+                    return boards.boardsContent_kor.contains(searchWord).or(boards.reports.reportsSummaryKor.contains(searchWord));
+                }
+                case WRITER -> {
+                    return boards.parents.parentsNickname.contains(searchWord);
+                }
+                case REPORTS_KEYWORD -> {
+                    return boards.reports.reportsKeywordsKor.contains(searchWord);
+                }
+                case TITLE_AND_CONTENT_AND_REPORTS -> {
+                    return boards.boardsTitle_kor.contains(searchWord).or(boards.boardsContent_kor.contains(searchWord)
+                            .or(boards.reports.reportsSummaryKor.contains(searchWord)).or(boards.reports.reportsKeywordsKor.contains(searchWord)));
+                }
+                default -> {
+                    return null;
+                }
+            }
+        }
+    }
+
+    private BooleanExpression searchVietnam(BoardsSearchType boardSearchType, String searchWord) {
+        if (searchWord == null || searchWord.isEmpty()) {
+            return null;
+        } else {
+            switch (boardSearchType) {
+                case TITLE -> {
+                    return boards.boardsTitle_viet.contains(searchWord);
+                }
+                case CONTENT_AND_REPORTS_SUMMARY -> {
+                    return boards.boardsContent_viet.contains(searchWord).or(boards.reports.reportsSummaryViet.contains(searchWord));
+                }
+                case WRITER -> {
+                    return boards.parents.parentsNickname.contains(searchWord);
+                }
+                case REPORTS_KEYWORD -> {
+                    return boards.reports.reportsKeywordsViet.contains(searchWord);
+                }
+                case TITLE_AND_CONTENT_AND_REPORTS -> {
+                    return boards.boardsTitle_viet.contains(searchWord).or(boards.boardsContent_viet.contains(searchWord)
+                            .or(boards.reports.reportsSummaryViet.contains(searchWord)).or(boards.reports.reportsKeywordsViet.contains(searchWord)));
+                }
+                default -> {
+                    return null;
+                }
+            }
+        }
+    }
+
+    private QBoardsListDto createQBoardsListDto(Language language) {
+        QBoardsListDto boardsListDto = null;
+
+        switch (language) {
+            case kor -> boardsListDto = new QBoardsListDto(boards.boardsId, boards.boardsTitle_kor,
+                    boards.boardsContent_kor, boards.likes, boards.createdAt, boards.reports.reportsSummaryKor,
+                    boards.reports.reportsKeywordsKor, boards.parents.parentsNickname);
+            case viet -> boardsListDto = new QBoardsListDto(boards.boardsId, boards.boardsTitle_viet,
+                    boards.boardsContent_viet, boards.likes, boards.createdAt, boards.reports.reportsSummaryViet,
+                    boards.reports.reportsKeywordsViet, boards.parents.parentsNickname);
+        }
+        return boardsListDto;
+    }
+}
