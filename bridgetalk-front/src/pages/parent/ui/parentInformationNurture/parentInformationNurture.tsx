32eaa3d933440cb169af129055bfd6d4c6d2817e
조한빈 <whgksqls9999@naver.com>
2024-05-13 09:42:21 +0900

    [Feat] 육아정보 리스트 요청 함수 작성

--- bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurture.tsx
+++ bridgetalk-front/src/pages/parent/ui/parentInformationNurture/parentInformationNurture.tsx
@@ -3,34 +3,17 @@ import * as S from '@/styles/parent/parentInformationNews.style';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { BackButton } from '@/shared';
+import { handleNurtureInfoList } from '../../model';
+import { useReportStore } from '../../store';
 
-interface News {
-  newsId: number;
-  newsTitle: string;
-  newsDescription: string;
-}
-
-export function ParentInformationNews() {
+export function ParentInformationNurture() {
   const navigate = useNavigate();
-  const [newsList, setNewsList] = useState<News[]>([]);
+  const [infoList, setInfoList] = useState<any[]>([]);
+
+  const language = useReportStore((state) => state.language);
 
   useEffect(() => {
-    const fetchData = [
-      {
-        newsId: 1,
-        newsTitle: 'Dòng vốn ròng trong ngày thứ ba từ Quỹ Anh',
-        newsDescription:
-          'Các quỹ đã được rút khỏi quỹ loại trong ngày thứ ba. Theo Hiệp hội Đầu tư Tài chính vào ngày 26, 12,6 tỷ won đã bị rò rỉ ròng từ các quỹ <b> cổ phiếu trong nước </b>, không bao gồm các quỹ thương mại trao đổi (ETF) vào ngày 22. 47,2 tỷ won đã được nhập và 59,8 tỷ won là quỹ... ',
-      },
-      {
-        newsId: 2,
-        newsTitle: 'Dòng vốn ròng trong ngày thứ ba từ Quỹ Anh',
-        newsDescription:
-          'Các quỹ đã được rút khỏi quỹ loại trong ngày thứ ba. Theo Hiệp hội Đầu tư Tài chính vào ngày 26, 12,6 tỷ won đã bị rò rỉ ròng từ các quỹ <b> cổ phiếu trong nước </b>, không bao gồm các quỹ thương mại trao đổi (ETF) vào ngày 22. 47,2 tỷ won đã được nhập và 59,8 tỷ won là quỹ... ',
-      },
-    ];
-
-    setNewsList(fetchData);
+    handleNurtureInfoList(language, setInfoList);
   }, []);
 
   return (
