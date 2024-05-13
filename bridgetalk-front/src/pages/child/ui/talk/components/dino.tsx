c256226ce37259ef30bae3a65a11a3f409f5f83c
조한빈 <whgksqls9999@naver.com>
2024-05-14 00:09:12 +0900

    [Feat] 감정에 따른 캐릭터 애니메이션 변화

--- bridgetalk-front/src/pages/child/ui/talk/components/dino.tsx
+++ bridgetalk-front/src/pages/child/ui/talk/components/dino.tsx
@@ -6,16 +6,19 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { useTalkStore } from '@/pages/child/store';
 import { postProfileLogin, useUserStore } from '@/pages/main';
 import { getUUIDbyToken } from '@/shared';
+import { getDinoEmotion } from '@/pages/child/model';
 
 extend({ OrbitControls });
 
 export function Dino() {
   const setIsTalking = useTalkStore((state) => state.setIsTalking);
+  const emotion = useTalkStore((state) => state.emotion);
   const isEnd = useTalkStore((state) => state.isEnd);
   const [size, setSize] = useState<number>(1);
-  const [dino, setDino] = useState<string>('D1');
+  const [dino, setDino] = useState<string>(sessionStorage.getItem('dino') ?? 'D1');
+  const [act, setAct] = useState('idle');
 
-  const gltf = useLoader(GLTFLoader, `/assets/dino/${dino}/happy.glb`);
+  const gltf = useLoader(GLTFLoader, `/assets/dino/${dino}/${act}.glb`);
   const mixer = useRef<AnimationMixer | null>(null);
 
   useEffect(() => {
@@ -34,6 +37,11 @@ export function Dino() {
   }, []);
 
   useEffect(() => {
+    setAct(getDinoEmotion(emotion));
+    console.log(getDinoEmotion(emotion));
+  }, [emotion]);
+
+  useEffect(() => {
     if (gltf.animations.length > 0) {
       mixer.current = new AnimationMixer(gltf.scene);
       const action = mixer.current.clipAction(gltf.animations[0]);
@@ -46,18 +54,18 @@ export function Dino() {
     };
   }, [gltf.animations, gltf.scene]);
 
-  useEffect(() => {
-    function onWindowResize(width: number) {
-      setSize(1);
-      console.log(size);
-    }
+  // useEffect(() => {
+  //   function onWindowResize(width: number) {
+  //     setSize(1);
+  //     console.log(size);
+  //   }
 
-    window.addEventListener('resize', () => onWindowResize(window.innerWidth));
+  //   window.addEventListener('resize', () => onWindowResize(window.innerWidth));
 
-    return () => {
-      window.removeEventListener('resize', () => onWindowResize(window.innerWidth));
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('resize', () => onWindowResize(window.innerWidth));
+  //   };
+  // }, []);
 
   useFrame((state, delta) => {
     mixer.current?.update(delta);
