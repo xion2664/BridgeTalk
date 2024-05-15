e227c34eea8244f252145cb75874c93b8894ffbf
조한빈 <whgksqls9999@naver.com>
2024-05-15 22:12:38 +0900

    [Feat] 부모 페이지 navbar

--- bridgetalk-front/src/pages/parent/ui/parent/parent.tsx
+++ bridgetalk-front/src/pages/parent/ui/parent/parent.tsx
@@ -1,6 +1,6 @@
 import * as S from '@/styles/parent/parent.style';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useReportStore } from '../../store';
 import { getSlang } from '../../query';
@@ -14,9 +14,47 @@ export function Parent() {
 
   return (
     <S.Background>
+      <Navbar navigate={navigate} />
       <Outlet />
       <LangIcon />
     </S.Background>
+  );
+}
+function Navbar({ navigate }: any) {
+  return (
+    <S.Navbar>
+      <button
+        onClick={() => {
+          navigate('main');
+        }}
+      >
+        <img src={`/assets/img/parent/navbar/home_off.svg`} />
+      </button>
+      <button
+        onClick={() => {
+          navigate('report');
+        }}
+      >
+        <img src={`/assets/img/parent/navbar/message_off.svg`} />
+      </button>
+      <button
+        onClick={() => {
+          navigate('information/nurture');
+        }}
+      >
+        <img src={`/assets/img/parent/navbar/nurture_off.svg`} />
+      </button>
+      <button
+        onClick={() => {
+          navigate('information/word');
+        }}
+      >
+        <img src={`/assets/img/parent/navbar/info_off.svg`} />
+      </button>
+      <button>
+        <img src={`/assets/img/parent/navbar/community_off.svg`} />
+      </button>
+    </S.Navbar>
   );
 }
 
--- bridgetalk-front/src/styles/parent/parent.style.ts
+++ bridgetalk-front/src/styles/parent/parent.style.ts
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
+import { color } from './common.style';
 
 export const Background = styled.div`
-  background-image: url('/assets/img/parent_bg.png');
-  background-size: cover;
-  background-position-x: 30%;
+  /* background-image: url('/assets/img/parent_bg.png'); */
+  /* background-size: cover;
+  background-position-x: 30%; */
+  background-color: ${color(1).sub};
   width: 100svw;
   height: 100svh;
 
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: start;
 
   overflow: hidden;
 
@@ -25,6 +26,34 @@ export const Background = styled.div`
 
     img {
       width: 5svw;
+    }
+  }
+`;
+
+export const Navbar = styled.div`
+  height: 100svh;
+  width: 10svw;
+  border: 1px solid black;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  button {
+    background-color: transparent;
+    border: none;
+
+    width: 100%;
+    height: 15svh;
+
+    img {
+      cursor: pointer;
+
+      transition: all 0.2s;
+
+      &:hover {
+        transform: scale(1.1);
+      }
     }
   }
 `;
