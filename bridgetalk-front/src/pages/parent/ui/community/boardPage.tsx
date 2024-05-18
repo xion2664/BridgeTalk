bf7da04ed070bad804265be47ccdcc6de4a5eff5
조한빈 <whgksqls9999@naver.com>
2024-05-19 01:05:28 +0900

    [Design] 커뮤니티 게시글 리스트 페이지 CSS 진행중

--- bridgetalk-front/src/pages/parent/ui/community/boardPage.tsx
+++ bridgetalk-front/src/pages/parent/ui/community/boardPage.tsx
@@ -1,30 +1,91 @@
+import { useEffect, useMemo, useState } from 'react';
 import { ArticleList } from './components/articleList';
 import * as S from '@/styles/parent/boardPage.style';
+import { useBoardStore, useReportStore } from '../../store';
+import { getBoardList } from '../../query';
 
-
+// infant_and_toddler', 'school', 'puberty'
 export function BoardPage() {
+  const language = useReportStore((state) => state.language);
+  const boardStore = useBoardStore();
+
+  const [page, setPage] = useState<number>(0);
+
+  function activeCategory(category: string, searchType: string) {
+    const map: any = {
+      제목: '제목',
+      작성자: '작성자',
+      내용: '내용과레포트요약',
+    };
+
+    return map[category] === searchType ? 'active' : '';
+  }
+  const categories = useMemo(
+    () => ({
+      kor: ['제목', '작성자', '내용'],
+      viet: [],
+      ph: [],
+    }),
+    [],
+  );
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getBoardList(language, page, '내용과레포트요약', '', boardStore.sortType);
+        console.log(data);
+      } catch (err) {}
+    }
+    fetchData();
+  }, [language]);
+
   return (
     <S.Container>
-    <div className="boardPage">
-      <div className="boardPage__header">
-        <div className="boardPage__header-title">
-          <h1>Community</h1>
-          <p>parents' talking</p>
+      <div className="boardPage">
+        <div className="boardPage__categories">
+          <button
+            className={`boardPage__categories-title ${activeCategory(boardStore.searchType, '제목')}`}
+            onClick={() => {
+              boardStore.setSearchType('제목');
+            }}
+          >
+            {categories[language][0]}
+          </button>
+          <button
+            className={`boardPage__categories-writer ${activeCategory(boardStore.searchType, '작성자')} `}
+            onClick={() => {
+              boardStore.setSearchType('작성자');
+            }}
+          >
+            {categories[language][1]}
+          </button>
+          <button
+            className={`boardPage__categories-content ${activeCategory(boardStore.searchType, '내용과레포트요약')}`}
+            onClick={() => {
+              boardStore.setSearchType('내용');
+            }}
+          >
+            {categories[language][2]}
+          </button>
         </div>
-        <div className="boardPage__header-side">
-          <button>New Article</button>
+        <div className="boardPage__search">
+          <form className="boardPage__search-form">
+            <input className="boardPage__search-input" type="text" placeholder="검색어를 입력해주세요"></input>
+            <button className="boardPage__search-button">
+              <img src={`/assets/img/parent/community/search_solid.svg`} />
+            </button>
+          </form>
         </div>
       </div>
-      <div className="boardPage__container">
-        <div className="boardPage__container-content">
-          <ArticleList />
-        </div>
-        <div className="boardPage__container-content">
-          <span>The List is Empty</span>
-          <span>Ask a Question and find your Answer!</span>
-        </div>
+      <div className="sort">
+        <button className="sort__latest"></button>
+        <button className="sort__popular"></button>
       </div>
-    </div>
+      <button className="write"></button>
     </S.Container>
   );
+}
+
+function BoardList() {
+  return <div className="boardPage__list"></div>;
 }
--- bridgetalk-front/src/styles/parent/boardPage.style.ts
+++ bridgetalk-front/src/styles/parent/boardPage.style.ts
@@ -2,116 +2,95 @@ import styled, { css } from 'styled-components';
 import { color, textShadowBlue } from './common.style';
 import { insetShadow } from '../main/common.style';
 
-const gridLayout = css`
-  display: grid;
-  grid-template-columns: 2fr 10fr 2fr;
-`;
-
 export const Container = styled.div`
+  * {
+    font-family: 'Pretendard';
+  }
+
   display: flex;
-  flex-direction: column;
-  height: 90svh;
-  width: 70svw;
-  gap: 2svh;
+  justify-content: center;
+  align-items: start;
+
+  width: 100%;
+  height: 100%;
+  padding: 2svh 2svw;
+
+  background-color: ${color(1).bright};
 
   .boardPage {
-    &__header {
-      display: ${gridLayout};
-      align-items: center;
-      gap: 2svw;
-      padding: 1svw 2svw;
+    display: flex;
+    flex-direction: column;
+    gap: 2svh;
 
-      &-title {
-        font-size: 2svw;
-        color: ${color(1).bright};
-        ${textShadowBlue};
+    width: 40svw;
+
+    &__categories {
+      display: flex;
+      gap: 1svw;
+
+      button {
+        font-size: 1.5svw;
+
+        color: ${color(1).black};
+        background-color: ${color(1).bright};
+
+        border: 1px solid ${color(1).line};
+        border-radius: 0.5svw;
+        padding: 1svh 0.5svw;
+
+        cursor: pointer;
+        transition: all 0.2s;
+
+        &:hover {
+          color: ${color(1).bright};
+          background-color: ${color(1).black};
+        }
+
+        &:active {
+          color: ${color(1).black};
+          background-color: ${color(1).bright};
+        }
       }
 
-      &-side {
-        button {
-          font-size: 1.5svw;
-          padding: 0.5svh 1svw;
-          border-radius: 0.5svw;
-          border: none;
-          background-color: ${color(1).sub};
-          cursor: pointer;
-          box-shadow: inset 0 0 0.5svh ${color(0.5).black};
-          transition: background-color 0.2s;
+      .active {
+        background-color: ${color(1).black};
 
-          &:hover {
-            background-color: ${color(1).bright};
+        color: ${color(1).bright};
+      }
+    }
+
+    &__search {
+      padding: 1.8svh 1.5svw;
+      border-top: 2px solid ${color(1).black};
+      border-bottom: 2px solid ${color(1).black};
+
+      &-form {
+        display: flex;
+        align-content: center;
+        justify-content: space-between;
+
+        input {
+          width: 100%;
+          font-size: 1.7svw;
+
+          border: none;
+          outline: none;
+        }
+
+        button {
+          padding: 1svh 0.5svw;
+
+          border-radius: 0.5svw;
+          border: 1px solid ${color(1).line};
+          background-color: transparent;
+
+          cursor: pointer;
+
+          img {
+            width: 1.7svw;
           }
         }
       }
-    }
-
-    &__container {
-      display: flex;
-      flex-direction: column;
-      gap: 2svh;
-
-      &-content {
-        padding: 2svh;
-        border-radius: 1svw;
-        background-color: ${color(1).light};
-        box-shadow: 0 0.5svh 0.4svh rgba(0, 0, 0, 0.5);
-
-        span {
-          font-size: 1.5svw;
-          color: ${color(0.5).black};
-        }
-      }
-    }
-  }
-`;
-
-export const ArticleList = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1svh;
-
-  .article {
-    background-color: white;
-    padding: 1svw;
-    border-radius: 0.5svw;
-    box-shadow: 0 0.4svh rgba(0, 0, 0, 0.2);
-    transition: transform 0.2s;
-
-    &:hover {
-      transform: translateY(-0.2svh);
-    }
-
-    &-title {
-      font-size: 1.5svw;
-      color: ${color(1).bright};
-    }
-
-    &-details {
-      font-size: 1.3svw;
-      color: ${color(0.5).black};
-    }
-  }
-`;
-
-export const Pagination = styled.div`
-  display: flex;
-  gap: 1svw;
-  justify-content: center;
-  align-items: center;
-  margin-top: 1svh;
-
-  button {
-    width: 3svw;
-    height: 3svw;
-    font-size: 1.5svw;
-    border-radius: 1svw;
-    border: none;
-    background-color: ${color(1).light};
-    cursor: pointer;
-
-    box-shadow: 0 0 1svh ${color(1).black};
-    &:hover {
-      background-color: ${color(1).sub};
     }
   }
 `;
