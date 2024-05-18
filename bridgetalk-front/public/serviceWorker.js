1fa621cd5a59df397ca39415bab87ca7ee14c628
조한빈 <whgksqls9999@naver.com>
2024-05-18 15:31:25 +0900

    [Feat] 양육정보, 슬랭 데이터 로컬 캐싱

--- bridgetalk-front/public/serviceWorker.js
+++ bridgetalk-front/public/serviceWorker.js
@@ -13,47 +13,31 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   console.log('fetched', e);
   if (e.request.method === 'GET') {
+    // 캐시 대상 요청이라면
     if (NEED_CACHE_URL.some((url) => e.request.url.includes(url))) {
-      const request_url = e.request.url;
-
-      console.log('{ 캐시 대상 요청 }');
-      // 캐싱 로직
-      // 1. 캐시 확인 후 정보가 있다면 해당 데이터 활용하기
-      // 2. 캐시 확인 후 정보가 없다면 요청 보내고 응답 데이터 활용하기
-
-      caches
-        .open(API_CACHE)
-        .then((CACHE_STORAGE) => {
-          CACHE_STORAGE.match(request_url).then((res) => {
-            if (!res) {
-              console.log('{ 캐싱 데이터 없음 }');
-              fetch(request_url).then((response) => {
-                console.log(`{ Fetch 리스폰스 ${response}}`);
-                CACHE_STORAGE.put(request_url, response);
-              });
-            } else if (res) {
-              console.log('{ 캐싱 데이터 있음 }');
-              return res;
+      e.respondWith(
+        caches.open(API_CACHE).then((CACHE_STORAGE) => {
+          return CACHE_STORAGE.match(e.request).then((cachedResponse) => {
+            if (cachedResponse) {
+              // 캐싱된 데이터가 있을 경우 캐시 데이터 반환
+              return cachedResponse;
+            } else {
+              // 캐싱된 데이터가 없을 경우 네트워크 요청을 수행
+              return fetch(e.request)
+                .then((networkResponse) => {
+                  if (networkResponse && networkResponse.status === 200) {
+                    // 캐시에 저장
+                    CACHE_STORAGE.put(e.request, networkResponse.clone());
+                  }
+                  return networkResponse;
+                })
+                .catch((error) => {
+                  throw error;
+                });
             }
           });
-        })
-        .catch((err) => console.log(err));
+        }),
+      );
     }
-
-    //     // e.responseWith(
-    //     //   caches.match(e.request).then((cachedResponse) => {
-    //     //     if (cachedResponse) {
-    //     //       return cachedResponse;
-    //     //     }
-    //     //     return fetch(e.request).then((networkResponse) => {
-    //     //       if (networkResponse.status === 200) {
-    //     //         caches.open(API_CACHE).then((cache) => {
-    //     //           cache.put(e.request, networkResponse.clone());
-    //     //         });
-    //     //       }
-    //     //       return networkResponse;
-    //     //     });
-    //     //   }),
-    //     // );
   }
 });
