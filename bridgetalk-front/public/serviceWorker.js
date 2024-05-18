5fba9e2dc8f8b775e1c0ef418cbcbf2b1eac3913
조한빈 <whgksqls9999@naver.com>
2024-05-18 09:00:12 +0900

    [Feat] 서비스 워커 수정중

--- bridgetalk-front/public/serviceWorker.js
+++ bridgetalk-front/public/serviceWorker.js
@@ -1,43 +1,53 @@
 const API_CACHE = 'api-cache-v1';
 
+const NEED_CACHE_URL = ['parentingInfo'];
+
 self.addEventListener('install', (e) => {
   console.log('installed', e);
 });
 
 self.addEventListener('activate', (e) => {
   console.log('activated', e);
-  const cacheWhiteList = [API_CACHE];
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhiteList.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        }),
-      );
-    }),
-  );
+  // const cacheWhiteList = [API_CACHE];
+  // e.waitUntil(
+  //   caches.keys().then((cacheNames) => {
+  //     return Promise.all(
+  //       cacheNames.map((cacheName) => {
+  //         if (!cacheWhiteList.includes(cacheName)) {
+  //           return caches.delete(cacheName);
+  //         }
+  //       }),
+  //     );
+  //   }),
+  // );
 });
 
 self.addEventListener('fetch', (e) => {
   console.log('fetched', e);
   if (e.request.method === 'GET') {
     console.log('GET 1');
-    e.responseWith(
-      caches.match(e.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return fetch(e.request).then((networkResponse) => {
-          if (networkResponse.status === 200) {
-            caches.open(API_CACHE).then((cache) => {
-              cache.put(e.request, networkResponse.clone());
-            });
-          }
-          return networkResponse;
-        });
-      }),
-    );
+
+    if (
+      NEED_CACHE_URL.some((url) => {
+        e.request.url.includes(url);
+      })
+    ) {
+    }
+
+    // e.responseWith(
+    //   caches.match(e.request).then((cachedResponse) => {
+    //     if (cachedResponse) {
+    //       return cachedResponse;
+    //     }
+    //     return fetch(e.request).then((networkResponse) => {
+    //       if (networkResponse.status === 200) {
+    //         caches.open(API_CACHE).then((cache) => {
+    //           cache.put(e.request, networkResponse.clone());
+    //         });
+    //       }
+    //       return networkResponse;
+    //     });
+    //   }),
+    // );
   }
 });
