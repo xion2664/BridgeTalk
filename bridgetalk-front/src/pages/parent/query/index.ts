21ef5c2b842f899d2482803fa4a1ed096e4c90a0
조한빈 <whgksqls9999@naver.com>
2024-05-19 12:46:20 +0900

    [Feat] 게시글 좋아요 함수 작성

--- bridgetalk-front/src/pages/parent/query/index.ts
+++ bridgetalk-front/src/pages/parent/query/index.ts
@@ -10,3 +10,4 @@ export * from '@/pages/parent/query/getBoardDetail/getBoardDetail';
 export * from '@/pages/parent/query/getCommentList/getCommentList';
 export * from '@/pages/parent/query/postBoardCreate/postBoardCreate';
 export * from '@/pages/parent/query/postCommentCreate/postCommentCreate';
+export * from '@/pages/parent/query/postBoardLike/postBoardLike';
