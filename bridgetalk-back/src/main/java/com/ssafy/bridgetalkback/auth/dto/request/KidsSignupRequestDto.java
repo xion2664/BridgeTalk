f0ed9f587f84651b4511f6b380df0a453675e287
sunkyoung <yskkjdh@gmail.com>
2024-05-23 15:53:19 +0900

    [Feat] requestDto 데이터 유효성 검사 항목 및 message 작성

--- bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/dto/request/KidsSignupRequestDto.java
+++ bridgetalk-back/src/main/java/com/ssafy/bridgetalkback/auth/dto/request/KidsSignupRequestDto.java
@@ -1,10 +1,17 @@
 package com.ssafy.bridgetalkback.auth.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record KidsSignupRequestDto(
+        @NotBlank(message = "부모Id는 필수입니다.")
         String parentsId,
+        @NotBlank(message = "이름은 필수입니다.")
         String kidsName,
+        @NotBlank(message = "닉네임은 필수입니다.")
         String kidsNickname,
+        @NotBlank(message = "공룡번호는 필수입니다.")
         String kidsDino,
+        @NotBlank(message = "비밀번호는 필수입니다.")
         String kidsPassword
 ) {
 }
